53718e3d3ec72cca73d21d9284732caf7b9a6a82
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-01-06 02:13:52 +0530

    Updated theme files

--- app/src/main/java/com/techTribeServices/suerHero/ui/theme/Type.kt
+++ app/src/main/java/com/techTribeServices/suerHero/ui/theme/Type.kt
@@ -16,36 +16,25 @@ val Cabin = FontFamily(
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Cabin,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     ),
-    titleLarge = TextStyle(
+    displayLarge = TextStyle(
+        fontFamily = Cabin,
+        fontWeight = FontWeight.Normal,
+        fontSize = 30.sp
+    ),
+    displayMedium = TextStyle(
         fontFamily = Cabin,
         fontWeight = FontWeight.Bold,
-        fontSize = 36.sp
+        fontSize = 20.sp
     ),
-    bodyMedium = TextStyle(
+    displaySmall = TextStyle(
         fontFamily = Cabin,
-        fontWeight = FontWeight.Normal,
+        fontWeight = FontWeight.Bold,
         fontSize = 20.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
